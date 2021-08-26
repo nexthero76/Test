446be68d67b5627d2e15eab51411b8bc9aa26fd9
Peter Yang <guangyang.nepsl@gmail.com>
2021-08-26 17:32:34 -0400

    Update csv.py

--- csv.py
+++ csv.py
@@ -1,6 +1,8 @@
 import os
 import pandas as pd
 
+
+# get csv file name list
 path = r"./Engineering Test Files"
 file_lists = []
 for _, _, files in os.walk(path):
@@ -9,7 +11,7 @@ for _, _, files in os.walk(path):
         if file_info[0] != "Combined" and file_info[1] == ".csv":
             file_lists.append(file_item)
             
-            
+# get "Source IP" and "name" lists         
 combined_dict={}
 for file_item in file_lists:
     path = r"./Engineering Test Files/" + file_item
@@ -22,7 +24,8 @@ ip_index = []
 for key in combined_dict.keys():
     first_elem = int(key.split('.')[0])
     ip_index.append(first_elem)
-
+    
+# update Combined.csv
 data2 = {"Source IP": combined_dict.keys(), "Environment": combined_dict.values()}
 combined_df = pd.DataFrame(data2,index=ip_index)
 combined_df.sort_index().to_csv("./Engineering Test Files/Combined.csv", index=False)
